c468edb1cf436639db414682d9bb69cc0ed263a3
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-01-11 01:27:12 +0530

    random provider

--- resource_group/resource_group.tf
+++ resource_group/resource_group.tf
@@ -2,6 +2,12 @@
 # Create a resource group
 
 resource "azurerm_resource_group" "tf-rg-1" {
-  name = "tf-rg1"
+  name     = "tf-rg1${random_string.myrandom.id}"
   location = "centralindia"
+}
+
+resource "random_string" "myrandom" {
+  length  = 16
+  special = false
+  upper   = false
 }
--- resource_group/versions.tf
+++ resource_group/versions.tf
@@ -2,9 +2,14 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = ">=2.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+
   }
 }
 
